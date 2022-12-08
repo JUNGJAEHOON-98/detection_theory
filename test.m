58d162e8fb02f346ba91b08a4c0d887c2c9ded51
JUNGJAEHOON <tree34005@gmail.com>
2022-12-08 23:31:50 +0900

    fail ZF-SIC ML & ZF & MMSE

--- test.m
+++ test.m
@@ -1,5 +1,5 @@
 clear;
-N = 10^4; % number of symbols
+N = 10^2; % number of symbols
 Eb_N0_dB = [0:3:30]; % multiple Eb/N0 values
 Nt = 4;
 Nr = 4;
@@ -13,6 +13,7 @@ for Eb_idx = 1:length(Eb_N0_dB)
     disp(Eb_N0_dB(Eb_idx));
     P = sqrt((10^(Eb_N0_dB(Eb_idx)/10))/Nt);
     x = P/sqrt(2) * x_; % normalization of energy to P
+    cnt_zfsic = 0;
     cnt_zf = 0;
     cnt_mmse = 0;
     cnt_ml = 0;
@@ -20,7 +21,7 @@ for Eb_idx = 1:length(Eb_N0_dB)
     % Channel and noise Noise addition
         h = 1/sqrt(2)*[randn(Nr, Nt) + 1j*randn(Nr, Nt)]; % Rayleigh channel
         n = 1/sqrt(2)*[randn(Nr,1) + 1j*randn(Nr,1)];  % white gaussian noise, 0dB variance
-        y = h*x(:,idx) + n;
+        y = h * x(:,idx) + n;
 
 
         w_zf = inv(h'* h) * h';
@@ -31,16 +32,19 @@ for Eb_idx = 1:length(Eb_N0_dB)
         zf_demod_ = qam_demod(w_zf * y);
         zf_demod = reshape(zf_demod_,[Nt, 1]);
 
+        zfsic_demod = zf_sic(w_zf, h, y, Nt);
+
         mmse_demod_ = qam_demod(w_mmse * y);
         mmse_demod = reshape(mmse_demod_,[Nt, 1]);
 
 
+        cnt_zfsic = cnt_zfsic + sum(x_(:,idx)~=zfsic_demod,"all");
         cnt_ml = cnt_ml + sum(x(:,idx)~=ml_demod,"all");
         cnt_zf = cnt_zf + sum(x_(:,idx)~=zf_demod,"all");
         cnt_mmse = cnt_mmse + sum(x_(:,idx)~=mmse_demod,"all");
 
     end
-
+    ser_zfsic(Eb_idx) = cnt_zfsic/N;
     ser_ml(Eb_idx) = cnt_ml/N;
     ser_zf(Eb_idx) = cnt_zf/N;
     ser_mmse(Eb_idx) = cnt_mmse/N;
@@ -52,7 +56,9 @@ hold on
 semilogy(Eb_N0_dB, ser_mmse, '^--','Color','#4DBEEE','LineWidth',2);
 hold on
 semilogy(Eb_N0_dB, ser_ml, '-','Color','#000000','LineWidth',2);
-legend('ZF', 'MMSE', 'ML');
+hold on
+semilogy(Eb_N0_dB, ser_zfsic, 'd-','Color','#EDB120','LineWidth',2);
+legend('ZF', 'MMSE', 'ML', 'ZF-SIC');
 xlabel('SNR[dB]')
 ylabel('SER');
 ylim([10^-3.5 10^0]);
@@ -93,4 +99,44 @@ function ipHat = qam_demod(input)
     ipHat(find(y_re > 0 & y_im > 0)) = (1+1*1j);
     ipHat(find(y_re < 0 & y_im > 0)) = (-1+1*1j);
     ipHat(find(y_re > 0 & y_im < 0)) = (1-1*1j);
+end
+
+
+function x_hat = zf_sic(w_zf, h, y, Nt)
+    w_norm1 = vecnorm(w_zf.');
+    [B,I1] = mink(w_norm1, 1,'ComparisonMethod','abs');
+    aa = w_zf(I1,:)*y;
+    x_hat1 = qam_demod(aa);
+
+    y1 = y-h(:, I1)*x_hat1;
+    h(:, I1) = 0;
+    w_zf2 = pinv(h);
+    w_norm2 = vecnorm(w_zf2.');
+    [B,I2] = mink(w_norm2, 2,'ComparisonMethod','abs');
+    bb = w_zf2(I2(2),:)*y1;
+    x_hat2 = qam_demod(bb);
+
+    y2 = y1-h(:, I2(2))*x_hat2;
+    h(:, I2(2)) = 0;
+    w_zf3 = pinv(h);
+    w_norm3 = vecnorm(w_zf3.');
+    [B,I3] = mink(w_norm3, 3,'ComparisonMethod','abs');
+    cc = w_zf3(I3(3),:)*y2;
+    x_hat3 = qam_demod(cc);
+
+    y3 = y2-h(:, I3(3))*x_hat3;
+    h(:, I3(3)) = 0;
+    w_zf4 = pinv(h);
+    w_norm4 = vecnorm(w_zf4.');
+    [B,I4] = mink(w_norm4, 4,'ComparisonMethod','abs');
+    dd = w_zf4(I4(4),:)*y3;
+    x_hat4 = qam_demod(dd);
+
+    x_hat = zeros([Nt,1]);
+    x_hat(I1) = x_hat1;
+    x_hat(I2(2)) = x_hat2;
+    x_hat(I3(3)) = x_hat3;
+    x_hat(I4(4)) = x_hat4;
+    disp(x_hat)
+
 end
